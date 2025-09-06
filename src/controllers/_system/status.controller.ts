9a6643f4abc99b3c6a6ec9982821dbf353220b6a
Andrew <vandrei1408@yahoo.com>
2025-09-06 12:07:33 +0300

    fix: remove unnecessary promise

--- src/controllers/_system/status.controller.ts
+++ src/controllers/_system/status.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
 
-export default async function statusController(_req: Request, res: Response) {
+export default function statusController(_req: Request, res: Response) {
   return res.status(200).json({ status: "ok" });
 }
